61db0168ecc294f9e521ae8f692686c63bac79b6
alex-sshao <alexs237044@gmail.com>
2026-06-20 23:03:05 -0400

    Feat: unsuccesfully worked on D, added notes

--- 2236/d.cpp
+++ 2236/d.cpp
@@ -3,12 +3,30 @@
 using namespace std;
 
 /* problem d notes:
-	 -- number of values > k doesn't really matter
-	- Even number of values < k: possible
-	- Odd number of values < k: Only if
+	- nevermind k does matter
+	- race to the bottom, can't choose value higher than chosen first. 
+	
 */
 
 int main(){
-	
-	
+	int tests = 0; 
+	cin >> tests;
+	while(tests--){
+		int k; int i; int t;
+		cin >> i;
+		cin >> k;
+		int s = i+1; int ns = 0;
+		int n = 0;
+		while(i--){
+			cin >> t;
+			if(t == s) ns++;
+			if(t < s){
+				if(s < i+1) n = s;
+				s = t;
+				ns = 1;
+			}
+		}
+		if(!(ns % 2) or n) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
 }
